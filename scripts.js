5292157fcc8cfe85af1416b903415d4af35f2d54
Sergey Liubomirov <104032225+fullxhate@users.noreply.github.com>
2024-05-26 15:28:24 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -89,28 +89,41 @@ document.getElementById('phone').addEventListener('input', function (e) {
 function closeOverlay(event) {
     var formOverlay = document.getElementById('formOverlay');
     var successOverlay = document.getElementById('successOverlay');
-    if (event.target == formOverlay) {
-        formOverlay.style.display = 'none';
-    }
-    if (event.target == successOverlay) {
-        successOverlay.style.display = 'none';
-    }
+    var target = event.target;
+
+    // Закрываем оверлей
+    formOverlay.style.display = 'none';
+    successOverlay.style.display = 'none';
+
+    // Отключаем реакцию на клики для элементов под оверлеем
+    formOverlay.style.pointerEvents = 'none';
+    successOverlay.style.pointerEvents = 'none';
+
+    // Устанавливаем таймер для включения реакции на клики после небольшой задержки
+    setTimeout(function() {
+        formOverlay.style.pointerEvents = 'auto';
+        successOverlay.style.pointerEvents = 'auto';
+    }, 100);
 }
 
 function stopPropagation(event) {
     event.stopPropagation();
 }
 
-document.getElementById('formOverlay').addEventListener('click', closeOverlay);
-document.getElementById('successOverlay').addEventListener('click', closeOverlay);
+
+
+
 document.getElementById('formOverlay').addEventListener('touchstart', closeOverlay);
 document.getElementById('successOverlay').addEventListener('touchstart', closeOverlay);
 
-document.querySelector('#formOverlay .form-container').addEventListener('click', stopPropagation);
-document.querySelector('#successOverlay .success-message').addEventListener('click', stopPropagation);
+
+
+
+
 document.querySelector('#formOverlay .form-container').addEventListener('touchstart', stopPropagation);
 document.querySelector('#successOverlay .success-message').addEventListener('touchstart', stopPropagation);
 
+// Обработчик отправки формы
 document.getElementById('transportForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Предотвратить отправку формы для демонстрационных целей
 
@@ -120,7 +133,7 @@ document.getElementById('transportForm').addEventListener('submit', function(eve
     // Показать сообщение об успешной отправке
     document.getElementById('successOverlay').style.display = 'flex';
 
-    // Скрыть сообщение об успешной отправке через 4 секунды
+    // Скрыть сообщение об успешной отправке через 2 секунды
     setTimeout(function() {
         document.getElementById('successOverlay').style.display = 'none';
     }, 2000);
@@ -152,3 +165,39 @@ document.addEventListener("DOMContentLoaded", function() {
 flatpickr('#date', {
     dateFormat: 'Y-m-d', // Формат даты
 });
+
+
+var formOverlay = document.getElementById('formOverlay');
+var bottomFix = document.getElementById('bottomFix');
+
+function hideBottomFix() {
+    bottomFix.style.transform = 'translateY(100%)';
+}
+
+function showBottomFix() {
+    bottomFix.style.transform = 'translateY(0)';
+}
+
+// Функция для блокировки прокрутки
+function toggleScrollLock() {
+    document.body.classList.toggle('no-scroll');
+}
+
+// Прослушиваем изменение стиля display для оверлея
+var observer = new MutationObserver(function(mutationsList, observer) {
+    for(var mutation of mutationsList) {
+        if (mutation.attributeName === 'style') {
+            var displayStyle = formOverlay.style.display;
+            if (displayStyle === 'flex') {
+                hideBottomFix();
+                toggleScrollLock(); // Блокируем прокрутку при открытии оверлея
+            } else if (displayStyle === 'none') {
+                showBottomFix();
+                toggleScrollLock(); // Разблокируем прокрутку при закрытии оверлея
+            }
+        }
+    }
+});
+
+
+observer.observe(formOverlay, { attributes: true });
